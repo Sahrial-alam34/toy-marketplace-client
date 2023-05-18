b59aac2a9ff372c05be8f985c56c72060c18e90d
Sahrial-alam34 <sunnykcp01@gmail.com>
2023-05-18 11:59:36 +0600

    header mobile responsive

--- src/pages/Home/Home/Banner/Banner.jsx
+++ src/pages/Home/Home/Banner/Banner.jsx
@@ -14,14 +14,14 @@ const Banner = () => {
                     Toy Truck is now available!!. Toy Bus is now available!!. Toy Car is now available!!. Toy Truck is now available!!.
                 </Marquee>
             </div>
-            <div className="carousel w-full h-[600px]">
+            <div className="carousel w-full h-full lg:h-[600px]">
 
                 <div id="slide1" className="carousel-item relative w-full">
                     <img src={img1} className="w-full rounded-xl" />
                     <div className="absolute rounded-xl h-full w-[1220px] flex items-center
              left-0 top-0  bg-gradient-to-r from-[#151515] to-[rgba(21,21,21,0.2)]">
-                        <div className='text-white space-y-7 pl-12 w-1/2'>
-                            <h2 className='text-6xl font-bold'>Make Kids Happy to Buy This Car</h2>
+                        <div className='text-white lg:space-y-7 lg:pl-12 w-1/2'>
+                            <h2 className='text-2xl lg:text-6xl font-bold'>Make Kids Happy to Buy This Car</h2>
                             <p>There are many variations of cars of  available, Choice your best one and make happy to your kids</p>
                             <div>
                                 <button className='btn btn-primary mr-5'>Discover More</button>
@@ -38,27 +38,26 @@ const Banner = () => {
                     <img src={img2} className="w-full rounded-xl" />
                     <div className="absolute rounded-xl h-full w-[1220px] flex items-center
              left-0 top-0  bg-gradient-to-r from-[#151515] to-[rgba(21,21,21,0.2)]">
-                        <div className='text-white space-y-7 pl-12 w-1/2'>
-                            <h2 className='text-6xl font-bold'>Make Kids Happy to Buy This Car</h2>
+                        <div className='text-white lg:space-y-7 lg:pl-12 w-1/2'>
+                            <h2 className='text-2xl lg:text-6xl font-bold'>Make Kids Happy to Buy This Car</h2>
                             <p>There are many variations of cars of  available, Choice your best one and make happy to your kids</p>
                             <div>
                                 <button className='btn btn-primary mr-5'>Discover More</button>
                                 <button className='btn btn-outline btn-success'>Latest Cars</button>
                             </div>
                         </div>
-                        <div className="absolute flex justify-end transform -translate-y-1/2 left-5 right-5 bottom-0">
-                            <a href="#slide1" className="btn btn-circle mr-5">❮</a>
-                            <a href="#slide3" className="btn btn-circle">❯</a>
-                        </div>
                     </div>
-
+                    <div className="absolute flex justify-end transform -translate-y-1/2 left-5 right-5 bottom-0">
+                        <a href="#slide1" className="btn btn-circle mr-5">❮</a>
+                        <a href="#slide3" className="btn btn-circle">❯</a>
+                    </div>
                 </div>
                 <div id="slide3" className="carousel-item relative w-full">
                     <img src={img3} className="w-full rounded-xl" />
                     <div className="absolute rounded-xl h-full w-[1220px] flex items-center
              left-0 top-0  bg-gradient-to-r from-[#151515] to-[rgba(21,21,21,0.2)]">
-                        <div className='text-white space-y-7 pl-12 w-1/2'>
-                            <h2 className='text-6xl font-bold'>Make Kids Happy to Buy This Car</h2>
+                        <div className='text-white lg:space-y-7 lg:pl-12 w-1/2'>
+                            <h2 className='text-2xl lg:text-6xl font-bold'>Make Kids Happy to Buy This Car</h2>
                             <p>There are many variations of cars of  available, Choice your best one and make happy to your kids</p>
                             <div>
                                 <button className='btn btn-primary mr-5'>Discover More</button>
@@ -75,8 +74,8 @@ const Banner = () => {
                     <img src={img4} className="w-full rounded-xl" />
                     <div className="absolute rounded-xl h-full w-[1220px] flex items-center
              left-0 top-0  bg-gradient-to-r from-[#151515] to-[rgba(21,21,21,0.2)]">
-                        <div className='text-white space-y-7 pl-12 w-1/2'>
-                            <h2 className='text-6xl font-bold'>Make Kids Happy to Buy This Car</h2>
+                        <div className='text-white lg:space-y-7 lg:pl-12 w-1/2'>
+                            <h2 className='text-2xl lg:text-6xl font-bold'>Make Kids Happy to Buy This Car</h2>
                             <p>There are many variations of cars of  available, Choice your best one and make happy to your kids</p>
                             <div>
                                 <button className='btn btn-primary mr-5'>Discover More</button>
